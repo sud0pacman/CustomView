a81f3d9c29785c511daf9ea304939b07746ce4ce
Muhammad <96905615+sud0pacman@users.noreply.github.com>
2026-06-04 16:11:57 +0500

    Initial Commit

---
+++ CustomView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomView
+//
+//  Created by Muhammad on 04/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomView/CustomViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewApp.swift
+//  CustomView
+//
+//  Created by Muhammad on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
